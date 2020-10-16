79489ce1a04f22b7031ba7fa768572f723839ca0
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2020-10-16 10:30:44 +0200

    first exercice done

---
+++ ex01.js
@@ -0,0 +1,28 @@
+function swapIndex (array, fromIndex, toIndex) {
+  // autre solution pour le swap :
+  // let element = array.splice(fromIndex, 1)[0];
+  //    array.splice(toIndex, 0, element)
+
+  let firstElement = array[fromIndex]
+  array[fromIndex] = array[toIndex]
+  array[toIndex] = firstElement
+}
+
+function bubbleSorting (arr) {
+  let swappedWasDone = false
+  for (i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      swappedWasDone = true
+      swapIndex(arr, i, i + 1)
+    }
+  }
+  if (swappedWasDone === true) {
+    bubbleSorting(arr)
+  }
+  // continuer à boucler si la condition est toujours bonne
+  // the pass through the list is repeated until no swaps are needed
+  return arr
+}
+
+let firstRes = bubbleSorting([5, 1, 4, 2, 8])
+console.log(firstRes)
